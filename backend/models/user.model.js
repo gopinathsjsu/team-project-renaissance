b9255afe1380c3e5bf3b57fe0d9e75cdd39dd067
unknown <govil.surabhi@gmail.com>
2021-04-09 16:29:19 -0700

    allow null value for user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,25 +1,32 @@
 module.exports = (sequelize, Sequelize) => {
     const Users = sequelize.define("users", {
         first_name: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: true
         },
         last_name: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: true
         },
         username: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         email: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: true
         },
         address: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         phone_number: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         password: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         }
     });
 
